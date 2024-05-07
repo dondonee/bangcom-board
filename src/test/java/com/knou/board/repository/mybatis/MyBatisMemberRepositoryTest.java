7c250a921bebef0fad3b1d91a87178919e56ba3b
dondonee <dyn.hwang@gmail.com>
2024-05-07 21:14:56 +0900

    #6 test: 회원가입 관련 Mapper 메서드 테스트
     - 아이디(loginName)은 case-insensitive
     - 닉네임(nickname)은 case-insensitive
     - 비밀번호(password) 관련 메서드는 암호화 적용 예정이라 테스트하지 않음

--- src/test/java/com/knou/board/repository/mybatis/MyBatisMemberRepositoryTest.java
+++ src/test/java/com/knou/board/repository/mybatis/MyBatisMemberRepositoryTest.java
@@ -3,7 +3,6 @@ package com.knou.board.repository.mybatis;
 import com.knou.board.domain.member.*;
 import com.knou.board.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +10,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 import static com.knou.board.domain.member.Member.*;
 import static org.assertj.core.api.Assertions.*;
@@ -48,5 +48,92 @@ public class MyBatisMemberRepositoryTest {
         Member selectMember = memberRepository.selectProfileById(member.getUserNo());
         assertThat(selectMember.getUserNo()).isEqualTo(member.getUserNo());
         assertThat(selectMember.getProfileId()).isEqualTo(member.getProfileId());
+    }
+
+    @Test
+    public void selectUserByLoginName() {
+        //given
+        Map<String, Object> memberMap = setMember();
+        MemberLogin memberLogin = (MemberLogin) memberMap.get("memberLogin");
+        memberLogin.setLoginName("knou01");
+        memberRepository.insertUser(memberLogin);
+
+        //when
+        MemberLogin findMember = memberRepository.selectUserByLoginName("knou01");
+
+        //then
+        assertThat(findMember).isNotNull();
+    }
+
+    @Test
+    public void selectUserByLoginName_CaseInsensitive_True() {
+        //given
+        Map<String, Object> memberMap = setMember();
+        MemberLogin memberLogin = (MemberLogin) memberMap.get("memberLogin");
+        memberLogin.setLoginName("knou01");
+        memberRepository.insertUser(memberLogin);
+
+        //when
+        MemberLogin findMember = memberRepository.selectUserByLoginName("KNOU01");  // 대문자로 검색
+
+        //then
+        assertThat(findMember).isNotNull();
+    }
+
+    @Test
+    public void selectProfileByNickName() {
+        //given
+        Map<String, Object> memberMap = setMember();
+        MemberLogin memberLogin = (MemberLogin) memberMap.get("memberLogin");
+        memberLogin = memberRepository.insertUser(memberLogin);
+
+        Member member = (Member) memberMap.get("member");
+        member.setUserNo(memberLogin.getUserNo());
+        member.setNickname("bangcom");
+        member = memberRepository.insertProfile(member);
+
+        //when
+        Member findMember = memberRepository.selectProfileByNickName("bangcom");
+
+        //then
+        assertThat(findMember).isNotNull();
+    }
+
+    @Test
+    public void selectProfileByNickName_CaseInsensitive_True() {
+        //given
+        Map<String, Object> memberMap = setMember();
+        MemberLogin memberLogin = (MemberLogin) memberMap.get("memberLogin");
+        memberLogin = memberRepository.insertUser(memberLogin);
+
+        Member member = (Member) memberMap.get("member");
+        member.setUserNo(memberLogin.getUserNo());
+        member.setNickname("bangcom");
+        member = memberRepository.insertProfile(member);
+
+        //when
+        Member findMember = memberRepository.selectProfileByNickName("BANGCOM"); // 대문자로 검색
+
+        //then
+        assertThat(findMember).isNotNull();
+    }
+
+
+    // private 메서드
+
+    private Map<String, Object> setMember() {
+        MemberLogin memberLogin = new MemberLogin();
+        memberLogin.setLoginName("loginName");
+        memberLogin.setPassword("password");
+
+        Member member = new Member();
+        member.setNickname("nickname");
+        member.setGrade(Grade.GRADE_1);
+        member.setTransferred(false);
+        member.setRegion(Region.SEOUL);
+        member.setAuthority(Authority.USER);
+        member.setJoinedDate(LocalDateTime.now());
+
+        return Map.of("memberLogin", memberLogin, "member", member);
     }
 }
